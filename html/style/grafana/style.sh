3a3dfa0bbdf213fc949386ed82a6e6ac4c9a1485
Dénes Csala <mail@csaladen.es>
2020-08-03 01:29:36 +0100

    adds menu

--- html/style/grafana/style.sh
+++ html/style/grafana/style.sh
@@ -1,5 +1,6 @@
-docker cp /home/ubuntu/html/style/grafana/dark.css grafana:/usr/share/grafana/public/build/grafana.dark.41bc4b193210d0f89.css
-docker cp /home/ubuntu/html/style/grafana/light.css grafana:/usr/share/grafana/public/build/grafana.light.41bc4b193210d0f89.css
+docker cp /home/ubuntu/html/style/grafana/dark.css grafana:/usr/share/grafana/public/build/grafana.dark.41bc.css
+docker cp /home/ubuntu/html/style/grafana/light.css grafana:/usr/share/grafana/public/build/grafana.light.41bc.css
 docker cp /home/ubuntu/html/style/grafana/index.html grafana:/usr/share/grafana/public/views/index.html
 docker cp /home/ubuntu/html/style/grafana/index.html grafana:/usr/share/grafana/public/views/index-template.html
+docker cp /home/ubuntu/html/style/grafana/dashboard.js grafana:/usr/share/grafana/public/build/DashboardPage.41bc4b193210d0f899b3.js
 docker restart grafana
