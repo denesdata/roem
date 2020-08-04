7ca95cbe62867fa0d3445e5343dd667aa58c0b91
Csala <csala@lancaster.ac.uk>
2020-08-04 21:28:33 +0100

    adds scroll menu, new panels

--- html/style/grafana/style.sh
+++ html/style/grafana/style.sh
@@ -1,6 +1,6 @@
-docker cp /home/ubuntu/html/style/grafana/dark.css grafana:/usr/share/grafana/public/build/grafana.dark.41bc.css
-docker cp /home/ubuntu/html/style/grafana/light.css grafana:/usr/share/grafana/public/build/grafana.light.41bc.css
+docker cp /home/ubuntu/html/style/grafana/dark.css grafana:/usr/share/grafana/public/build/grafana.dark.YOUR_BUILD_STRING_HERE.css
+docker cp /home/ubuntu/html/style/grafana/light.css grafana:/usr/share/grafana/public/build/grafana.light.YOUR_BUILD_STRING_HERE.css
 docker cp /home/ubuntu/html/style/grafana/index.html grafana:/usr/share/grafana/public/views/index.html
 docker cp /home/ubuntu/html/style/grafana/index.html grafana:/usr/share/grafana/public/views/index-template.html
-docker cp /home/ubuntu/html/style/grafana/dashboard.js grafana:/usr/share/grafana/public/build/DashboardPage.41bc4b193210d0f899b3.js
+docker cp /home/ubuntu/html/style/grafana/dashboard.js grafana:/usr/share/grafana/public/build/DashboardPage.YOUR_BUILD_STRING_HERE.js
 docker restart grafana
